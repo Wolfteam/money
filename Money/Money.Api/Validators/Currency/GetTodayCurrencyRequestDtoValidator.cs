e0eec6e579257fa12ff9895c9e500659d5c8ba62
Efrain <mimo4325@gmail.com>
2021-04-27 21:35:18 -0500

    [Api] Added a few validators

---
+++ Money/Money.Api/Validators/Currency/GetTodayCurrencyRequestDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Money.Api.Dto.Currency.Requests;
+using Money.Api.Extensions;
+
+namespace Money.Api.Validators.Currency
+{
+    public class GetTodayCurrencyRequestDtoValidator : AbstractValidator<GetTodayCurrencyRequestDto>
+    {
+        public GetTodayCurrencyRequestDtoValidator()
+        {
+            RuleFor(dto => dto.Type)
+                .SupportedCurrencyType();
+        }
+    }
+}
---
+++ Money/Money.Api/Validators/Transactions/CreateTransactionRequestDtoValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Money.Api.Dto.Transactions.Requests;
+using Money.Api.Extensions;
+
+namespace Money.Api.Validators.Transactions
+{
+    public class CreateTransactionRequestDtoValidator : AbstractValidator<CreateTransactionRequestDto>
+    {
+        public CreateTransactionRequestDtoValidator()
+        {
+            RuleFor(dto => dto.UserId)
+                .GreaterThan(0)
+                .WithGlobalErrorCode();
+
+            RuleFor(dto => dto.AmountToPay)
+                .GreaterThan(0)
+                .WithGlobalErrorCode();
+
+            RuleFor(dto => dto.CurrencyType)
+                .SupportedCurrencyType();
+        }
+    }
+}
