1a1f4afc0d1fb02e1accda59c3c8da0d4795864f
Efrain <mimo4325@gmail.com>
2021-04-28 08:45:21 -0500

    [Api] Added a merge migration

---
+++ Money/Money.Api/Migrations/20210427035833_Init.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Money.Api.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedBy = table.Column<string>(maxLength: 150, nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<string>(maxLength: 150, nullable: true),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedBy = table.Column<string>(maxLength: 150, nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<string>(maxLength: 150, nullable: true),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    CurrencyType = table.Column<int>(nullable: false),
+                    PaidAmount = table.Column<double>(nullable: false),
+                    PurchasedAmount = table.Column<double>(nullable: false),
+                    UserId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transactions_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CreatedAt", "CreatedBy", "LastName", "Name", "UpdatedAt", "UpdatedBy" },
+                values: new object[] { 1L, new DateTime(2021, 4, 26, 22, 58, 33, 84, DateTimeKind.Local).AddTicks(7402), "system", "Bastidas", "Efrain", null, null });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CreatedAt", "CreatedBy", "LastName", "Name", "UpdatedAt", "UpdatedBy" },
+                values: new object[] { 2L, new DateTime(2021, 4, 26, 22, 58, 33, 85, DateTimeKind.Local).AddTicks(5009), "system", "Bastidas", "Elena", null, null });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_UserId",
+                table: "Transactions",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
