6e1810b84b47111a525104be1270f6ee9c95eb5e
Efrain <mimo4325@gmail.com>
2021-04-27 21:36:44 -0500

    [Api] Added initial controllers

---
+++ Money/Money.Api/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Money.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class BaseController<T> : ControllerBase
+    {
+        protected readonly ILogger<T> Logger;
+
+        protected BaseController(ILogger<T> logger)
+        {
+            Logger = logger;
+        }
+    }
+}
---
+++ Money/Money.Api/Controllers/CurrencyController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Money.Api.Dto.Currency.Requests;
+using Money.Api.Dto.Transactions.Requests;
+using Money.Api.Interfaces;
+using System.Threading.Tasks;
+
+namespace Money.Api.Controllers
+{
+    public class CurrencyController : BaseController<CurrencyController>
+    {
+        private readonly IPriceProvider _priceProvider;
+        private readonly ITransactionService _transactionService;
+
+        public CurrencyController(
+            ILogger<CurrencyController> logger,
+            IPriceProvider priceProvider,
+            ITransactionService transactionService)
+            : base(logger)
+        {
+            _priceProvider = priceProvider;
+            _transactionService = transactionService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTodayPrice([FromQuery] GetTodayCurrencyRequestDto dto)
+        {
+            Logger.LogInformation($"Getting today's price for currency = {dto.Type}...");
+            var response = await _priceProvider.GetTodayPrice(dto);
+
+            Logger.LogInformation($"The price for today's currency is = {response.Result}");
+            return Ok(response);
+        }
+
+        [HttpPost("Purchase")]
+        public async Task<IActionResult> CreateTransaction(CreateTransactionRequestDto dto)
+        {
+            Logger.LogInformation($"Trying to create a new transaction for userId = {dto.UserId}...");
+            var response = await _transactionService.CreateTransaction(dto);
+
+            Logger.LogInformation($"UserId = {dto.UserId} puchased = {response.Result.PurchasedAmount} ({dto.CurrencyType})");
+            return Ok(response);
+        }
+    }
+}
